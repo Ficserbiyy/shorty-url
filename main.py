12cbbfe178053cc33c94fc84bb4ef2b6a8957577
ficserbiYY <ficserbiy@gmail.com>
2026-06-01 16:58:57 +0500

    feat: create redirect endpoint

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Depends, HTTPException, Request, status
-from typing import Final
+from sqlmodel import select
+from typing import Final, List
 from Logic import encode_base62
 from sqlalchemy.orm import joinedload
 import redis.asyncio as redis
@@ -8,7 +9,7 @@ from fastapi.encoders import jsonable_encoder
 from dbLogic import get_session, create_db_and_tables, AsyncSession, engine
 from contextlib import asynccontextmanager
 from models import URL, UrlBase, UrlResponse, UrlUpdate
-
+from fastapi.responses import RedirectResponse
 
 
 
@@ -26,11 +27,12 @@ redis_client: Final = redis.from_url("redis://localhost:6379", decode_responses=
 
 @app.post('/shorten', response_model=UrlResponse)
 async def short_url(
-    url_data: UrlBase, 
-    session: AsyncSession = Depends(get_session),
-    request: Request
+    url_data: UrlBase,
+    request: Request, 
+    session: AsyncSession = Depends(get_session)
 ):
-    client_ip = request.client.host
+    'Create Short URL'
+    client_ip = request.client.host if request.client else "127.0.0.1"
     limit_key = f"limit:urls:{client_ip}"
     current_count = await redis_client.incr(limit_key)
     
@@ -44,6 +46,7 @@ async def short_url(
     await session.commit()
     await session.refresh(db_url)
     
+    
     shortcode = encode_base62(db_url.id)
     db_url.shortcode = shortcode
     
@@ -52,5 +55,23 @@ async def short_url(
     await redis_client.set(f"url:{shortcode}", db_url.url)
     return db_url
     
+@app.get('/{shortcode}')
+async def redirect_to_url(shortcode: str, session: AsyncSession = Depends(get_session)):
+    'Redirect to Original URL'
+    cached_url = await redis_client.get(f"url:{shortcode}")
+    if cached_url:
+        if isinstance(cached_url, str):
+            await redis_client.incr(f"stats:{shortcode}")
+            return RedirectResponse(url=cached_url)
+    statement = select(URL).where(URL.shortcode == shortcode)
+    result = await session.execute(statement)
+    
+    db_url = result.scalars().first() 
+    if not db_url:
+        raise HTTPException(status_code=404, detail="Short URL not found")
+    
+    await redis_client.set(f"url:{shortcode}", db_url.url)
+    await redis_client.incr(f"stats:{shortcode}")
+    return RedirectResponse(url=db_url.url)
     
 
