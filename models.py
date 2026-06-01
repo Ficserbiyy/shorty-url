e6bd7d80db1da5c0ba02c6e95d9198b4bee6855f
ficserbiYY <ficserbiy@gmail.com>
2026-06-01 22:57:22 +0500

    refactor: add docker infrastructure

--- models.py
+++ models.py
@@ -21,13 +21,19 @@ class UrlResponse(UrlBase):
     access_count: int = 0
     
 class Settings(BaseSettings):
-    DATABASE_URL: str = ''
+    DB_USER: str = "postgres"
+    DB_PASSWORD: str = "password"
+    DB_HOST: str = "db" 
+    DB_NAME: str = "shorty"
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding='utf-8',
         extra='ignore',
         case_sensitive=False
     )
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}/{self.DB_NAME}"
 
 settings = Settings()
     
